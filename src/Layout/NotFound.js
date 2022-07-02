b4510591f8462f9aacfafd4bdb102d76bdb2e508
Grace Chung <gracehojung@hotmail.com>
2022-07-02 00:28:49 -0400

    first commit

---
+++ src/Layout/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="NotFound">
+      <h1 className="display-3">Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
---
+++ src/Layout/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Home from "./Home/Home";
+import Study from "./Study/StudyIndex";
+import NotFound from "./NotFound";
+import Header from "./Header";
+import EditDeck from "./Deck/EditDeck";
+import DeckView from "./Deck/DeckView";
+import EditCard from "./Card/EditCard";
+import CreateCard from "./Card/CreateCard";
+import CreateDeck from "./Deck/CreateDeck";
+
+function Layout() {
+	return (
+		<>
+			<Header />
+			<div className="container">
+				<Switch>
+					<Route path="/decks/new">
+						<CreateDeck />
+					</Route>
+					<Route path="/decks/:deckId/study">
+						<Study />
+					</Route>
+					<Route path="/decks/:deckId/edit">
+						<EditDeck />
+					</Route>
+					<Route path="/decks/:deckId/cards/new">
+						<CreateCard />
+					</Route>
+					<Route path="/decks/:deckId/cards/:cardId/edit">
+						<EditCard />
+					</Route>
+					<Route exact={true} path="/decks/:deckId">
+						<DeckView />
+					</Route>
+					<Route exact={true} path="/decks">
+						<Redirect to="/" />
+					</Route>
+					<Route exact={true} path="/">
+						<Home />
+					</Route>
+					<Route>
+						<NotFound />
+					</Route>
+				</Switch>
+			</div>
+		</>
+	);
+}
+
+export default Layout;
